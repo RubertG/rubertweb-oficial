fd8681a8246698b824d6b2bf859707e0c9e2aa7b
Rubert_G <rubertgonzalez5@gmail.com>
2023-07-24 11:57:49 -0500

    UPDATE: Types Tecnologies, const lista de tecnologias

---
+++ src/consts/consts.tsx
@@ -0,0 +1,13 @@
+import { CSS, Figma, Firebase, HTML, JS, React, Sass, TypeScript } from '@/components/Icons/Icons'
+import { type TecnologyType } from '@/types/types'
+
+export const TECNOLOGIES: TecnologyType[] = [
+  { name: 'HTML', icon: <HTML /> },
+  { name: 'CSS', icon: <CSS /> },
+  { name: 'JavaScript', icon: <JS /> },
+  { name: 'React', icon: <React /> },
+  { name: 'TypeScript', icon: <TypeScript /> },
+  { name: 'Sass', icon: <Sass /> },
+  { name: 'Firebase', icon: <Firebase /> },
+  { name: 'Figma', icon: <Figma /> }
+]
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,4 +1,13 @@
+import { type TECNOLOGIES } from '@/consts/consts'
+
+export interface TecnologyType {
+  name: string
+  icon: JSX.Element
+}
+
 export interface LinkNav {
   literal: string
   href: string
 }
+
+export type TecnologiesType = keyof typeof TECNOLOGIES
