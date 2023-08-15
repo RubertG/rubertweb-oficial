812e0f5c7a5ece0ef1b60e4ade57d6fc58008d15
Rubert_G <rubertgonzalez5@gmail.com>
2023-08-15 15:10:39 -0500

    UPDATE: Separacion de logica del nav en un Hook

--- src/components/layout/Nav/Nav.tsx
+++ src/components/layout/Nav/Nav.tsx
@@ -6,6 +6,7 @@ import Styles from './Nav.module.scss'
 import { useEffect, useRef, useState } from 'react'
 import { Arrow } from '@/components/Icons/Icons'
 import { usePathname } from 'next/navigation'
+import { useHoverAnimation } from '@/hooks/useHoverAnimation'
 
 interface Props {
   items: LinkNav[]
@@ -13,37 +14,9 @@ interface Props {
 
 function Nav({ items }: Props): JSX.Element {
   const pathname = usePathname()
+  const { hoverLiRef, liRefs } = useHoverAnimation()
   const [toggler, setToggler] = useState(false)
   const navRef = useRef<HTMLDivElement>(null)
-  const liRefs = useRef<Array<HTMLLIElement | null>>([])
-  const hoverDiv = useRef<HTMLLIElement>(null)
-
-  useEffect(() => {
-    if (window !== undefined) {
-      const lis: Array<HTMLLIElement | null> = liRefs.current
-      hoverAnimation(lis[0] as HTMLLIElement)
-
-      const mouseEnterHandler = (item: HTMLLIElement): void => {
-        hoverAnimation(item)
-        if (hoverDiv.current !== null) hoverDiv.current.style.opacity = '1'
-      }
-
-      const mouseLeaveHandler = (): void => {
-        if (hoverDiv.current !== null) hoverDiv.current.style.opacity = '0'
-      }
-
-      lis.forEach((item) => {
-        item?.addEventListener('mouseenter', () => { mouseEnterHandler(item) })
-        item?.addEventListener('mouseleave', mouseLeaveHandler)
-      })
-      return () => {
-        lis.forEach((item) => {
-          item?.removeEventListener('mouseenter', () => { mouseEnterHandler(item) })
-          item?.removeEventListener('mouseleave', mouseLeaveHandler)
-        })
-      }
-    }
-  }, [])
 
   useEffect(() => {
     if (window !== undefined) {
@@ -60,17 +33,6 @@ function Nav({ items }: Props): JSX.Element {
       }
     }
   }, [])
-
-  const hoverAnimation = (item: HTMLLIElement): void => {
-    const { top, left, width, height } = item.getBoundingClientRect()
-
-    if (hoverDiv.current !== null) {
-      hoverDiv.current.style.setProperty('--top', `${top}px`)
-      hoverDiv.current.style.setProperty('--left', `${left}px`)
-      hoverDiv.current.style.setProperty('--width', `${width}px`)
-      hoverDiv.current.style.setProperty('--height', `${height}px`)
-    }
-  }
 
   const handleClick = (): void => {
     setToggler(!toggler)
@@ -98,7 +60,7 @@ function Nav({ items }: Props): JSX.Element {
           <li
             className={Styles.hover_div}
             id='hover_div'
-            ref={hoverDiv}>
+            ref={hoverLiRef}>
           </li>
           {
             items.map(({ literal, href }) => {
---
+++ src/hooks/useHoverAnimation.ts
@@ -0,0 +1,55 @@
+import type React from 'react'
+import { useEffect, useRef } from 'react'
+
+interface Return {
+  liRefs: React.MutableRefObject<Array<HTMLLIElement | null>>
+  hoverLiRef: React.MutableRefObject<HTMLLIElement | null>
+}
+
+export function useHoverAnimation (): Return {
+  const liRefs = useRef<Array<HTMLLIElement | null>>([])
+  const hoverLiRef = useRef<HTMLLIElement>(null)
+
+  useEffect(() => {
+    if (window !== undefined) {
+      const lis: Array<HTMLLIElement | null> = liRefs.current
+      hoverAnimation(lis[0] as HTMLLIElement)
+
+      const mouseEnterHandler = (item: HTMLLIElement): void => {
+        hoverAnimation(item)
+        if (hoverLiRef.current !== null) hoverLiRef.current.style.opacity = '1'
+      }
+
+      const mouseLeaveHandler = (): void => {
+        if (hoverLiRef.current !== null) hoverLiRef.current.style.opacity = '0'
+      }
+
+      lis.forEach((item) => {
+        item?.addEventListener('mouseenter', () => { mouseEnterHandler(item) })
+        item?.addEventListener('mouseleave', mouseLeaveHandler)
+      })
+      return () => {
+        lis.forEach((item) => {
+          item?.removeEventListener('mouseenter', () => { mouseEnterHandler(item) })
+          item?.removeEventListener('mouseleave', mouseLeaveHandler)
+        })
+      }
+    }
+  }, [])
+
+  const hoverAnimation = (item: HTMLLIElement): void => {
+    const { top, left, width, height } = item.getBoundingClientRect()
+
+    if (hoverLiRef.current !== null) {
+      hoverLiRef.current.style.setProperty('--top', `${top}px`)
+      hoverLiRef.current.style.setProperty('--left', `${left}px`)
+      hoverLiRef.current.style.setProperty('--width', `${width}px`)
+      hoverLiRef.current.style.setProperty('--height', `${height}px`)
+    }
+  }
+
+  return {
+    liRefs,
+    hoverLiRef
+  }
+}
